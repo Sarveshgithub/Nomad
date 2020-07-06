1e50bc2783449c64774623c332b94cf1df8cc50b
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-07-06 18:50:52 +0530

    done validation

--- client/src/component/Register.js
+++ client/src/component/Register.js
@@ -1,19 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 function Register(props) {
-  const [userdetail, setUserDetail] = useState({
+  const [user, setUser] = useState({
     name: "",
     email: "",
     password: "",
+    cpass: "",
   });
-  function onchange(val) {
-    console.log("val:::", val.value, val.name);
-  }
-  console.log("userdetail", userdetail);
+  const [error, setError] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const onchange = (event) => {
+    const { name, value } = event.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+  const validation = () => {
+    const tempErr = {};
+
+    if (!user.name) {
+      tempErr["name"] = "Required";
+    }
+    if (!user.email) {
+      tempErr["email"] = "Required";
+    } else if (
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.email)
+    ) {
+      tempErr["email"] = "Invalid Email";
+    }
+    if (!user.password) {
+      tempErr["password"] = "Required";
+    } else if (user.password.length < 6) {
+      tempErr["password"] = "Password must be 6 character";
+    }
+    if (!user.cpass) {
+      tempErr["cpass"] = "Required";
+    } else if (user.password !== user.cpass) {
+      tempErr["cpass"] = "Password not matched";
+    }
+    return tempErr;
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setError(validation());
+    setIsSubmitting(true);
+  };
+  useEffect(() => {
+    if (Object.keys(error).length === 0 && isSubmitting) {
+      registerUser();
+    }
+  }, [error]);
+
+  const registerUser = () => {
+    console.log("register user");
+  };
   return (
     <section>
       <div className="wrap">
-        <form className="login-form">
+        <form className="login-form" onSubmit={submitHandler} noValidate>
           <div className="form-header">
             <h3>Sign Up</h3>
           </div>
@@ -23,9 +68,10 @@ function Register(props) {
               name="name"
               className="form-input"
               placeholder="Name"
-              onChange={(e) => onchange(e.target)}
+              value={user.name}
+              onChange={onchange}
             />
-            <span>{userdetail.name ? "Name Required field" : ""}</span>
+            {error.name && <p className="redColor">{error.name}</p>}
           </div>
           <div className="form-group">
             <input
@@ -33,8 +79,10 @@ function Register(props) {
               name="email"
               className="form-input"
               placeholder="Email"
-              onChange={(e) => onchange(e.target)}
+              value={user.email}
+              onChange={onchange}
             />
+            {error.email && <p className="redColor">{error.email}</p>}
           </div>
           <div className="form-group">
             <input
@@ -42,16 +90,21 @@ function Register(props) {
               name="password"
               className="form-input"
               placeholder="Password"
-              onChange={(e) => onchange(e.target)}
+              value={user.password}
+              onChange={onchange}
             />
+            {error.password && <p className="redColor">{error.password}</p>}
           </div>
           <div className="form-group">
             <input
               type="password"
-              name="cpassword"
+              name="cpass"
               className="form-input"
               placeholder="Confirm Password"
+              value={user.cpass}
+              onChange={onchange}
             />
+            {error.cpass && <p className="redColor">{error.cpass}</p>}
           </div>
           <div className="form-group">
             <button className="form-button" type="submit">
