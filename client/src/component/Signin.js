15148057bc2ebf68f825b87edd3446eac0fdcde7
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-09-15 15:05:58 +0530

    update components

--- client/src/component/Signin.js
+++ client/src/component/Signin.js
@@ -1,22 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 function Signin() {
+  const [user, setUser] = useState({
+    userName: "",
+    password: "",
+    secToken: "",
+    orgType: "",
+  });
+  const [error, setError] = useState({});
+  const [serverError, setServerError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const onchange = (event) => {
+    const { name, value } = event.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+  const validation = () => {
+    const tempErr = {};
+    if (!user.userName) {
+      tempErr["userName"] = "Required";
+    }
+    if (!user.password) {
+      tempErr["password"] = "Required";
+    }
+    if (!user.secToken) {
+      tempErr["secToken"] = "Required";
+    }
+    if (!user.orgType) {
+      tempErr["orgType"] = "Required";
+    }
+    return tempErr;
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setError(validation());
+    setIsSubmitting(true);
+  };
+  useEffect(() => {
+    if (Object.keys(error).length === 0 && isSubmitting) {
+      loginUser();
+    }
+  }, [error]);
+  const loginUser = () => {
+    console.log("test::", user);
+    axios
+      .post("/api/user/login", user)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        if (error) {
+          const {
+            response: {
+              data: { status, message },
+            },
+          } = error;
+          setServerError(message);
+        }
+      });
+  };
   return (
     <section>
       <div className="wrap">
-        <form className="login-form">
+        <form className="login-form" onSubmit={submitHandler} noValidate>
           <div className="form-header">
             <h3>Login</h3>
+            {serverError && <p className="redColor">{serverError}</p>}
           </div>
           <div className="form-group">
-            <input type="text" className="form-input" placeholder="Email" />
+            <input
+              type="text"
+              className="form-input"
+              name="userName"
+              value={user.userName}
+              onChange={onchange}
+              placeholder="UserName"
+            />
+            {error.userName && <p className="redColor">{error.userName}</p>}
           </div>
           <div className="form-group">
             <input
               type="password"
               className="form-input"
               placeholder="Password"
+              name="password"
+              value={user.password}
+              onChange={onchange}
             />
+            {error.password && <p className="redColor">{error.password}</p>}
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-input"
+              placeholder="Security Token"
+              name="secToken"
+              value={user.secToken}
+              onChange={onchange}
+            />
+            {error.secToken && <p className="redColor">{error.secToken}</p>}
+          </div>
+          <div className="form-group">
+            <input
+              type="radio"
+              value="https://login.salesforce.com"
+              name="orgType"
+              onChange={onchange}
+            />
+            {"Production"}
+            <input
+              type="radio"
+              value="https://test.salesforce.com"
+              name="gender"
+              name="orgType"
+              onChange={onchange}
+            />
+            {"Sandbox"}
+            {error.orgType && <p className="redColor">{error.orgType}</p>}
           </div>
           <div className="form-group">
             <button className="form-button" type="submit">
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const jsforce = require("jsforce");
 const router = express.Router();
-
+// const username = "sarvesh.kumar@sfdx.com";
+// const password = "Apple@123";
+// const token = "6LnwuvlLTfrE5gnaEe5UYK6PJ";
 router.post("/login", async (req, res) => {
   try {
     console.log("response:::", req.body);
-    const { userName, password, token } = req.body;
+    const { userName, password, secToken, orgType } = req.body;
     const conn = new jsforce.Connection({
-      loginUrl: "https://login.salesforce.com",
+      loginUrl: orgType,
     });
-    const response = await conn.login(userName, password + token);
+    const response = await conn.login(userName, password + secToken);
     response["accessToken"] = conn.accessToken;
     response["instanceUrl"] = conn.instanceUrl;
     res.send(response);
