d7e0116359cf33e22dd2df3aed4536fdd326c90c
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-10-22 20:18:52 +0530

    make it better

--- server/server.js
+++ server/server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const { MONGODB_URL } = require("./config");
 const userRoute = require("./routes/userRoute");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 app.use(bodyParser.json());
 app.use("/api/user", userRoute);
-app.listen(5000, () => {
+app.use(express.static(path.join(__dirname, "../build")));
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("server start");
 });
