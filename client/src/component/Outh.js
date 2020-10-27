2af4f8fb01aabd1116aa4b86ab034fbca9093a98
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-10-27 20:54:44 +0530

    changes

--- client/src/component/Outh.js
+++ client/src/component/Outh.js
@@ -30,26 +30,28 @@ function Outh(props) {
   useEffect(() => {
     if (Object.keys(error).length === 0 && isSubmitting) {
       console.log("test::", user);
-      axios
-        .get("/sflogin", user)
-        .then((response) => {
-          console.log("response::", response);
-          //   props.history.push({
-          //     pathname: "/home",
-          //     data: response.data,
-          //   });
-        })
-        .catch((error) => {
-          console.log("error::", error.response);
-          //   if (error) {
-          //     const {
-          //       response: {
-          //         data: { message },
-          //       },
-          //     } = error;
-          //     setServerError(message);
-          //   }
-        });
+      console.log("sdfsdfsd");
+      window.location = "/sflogin";
+      // axios
+      //   .get("http://localhost:5000/sflogin", user)
+      //   .then((response) => {
+      //     console.log("response::", response);
+      //     //   props.history.push({
+      //     //     pathname: "/home",
+      //     //     data: response.data,
+      //     //   });
+      //   })
+      //   .catch((error) => {
+      //     console.log("error::", error.response);
+      //     //   if (error) {
+      //     //     const {
+      //     //       response: {
+      //     //         data: { message },
+      //     //       },
+      //     //     } = error;
+      //     //     setServerError(message);
+      //     //   }
+      //   });
     }
     // eslint-disable-line react-hooks/exhaustive-deps
   }, [error, isSubmitting, user, props.history]);
