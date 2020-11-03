e0bbdce56bcfe03880a6056422779a13f1a066fb
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-11-04 00:13:52 +0530

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import Signin from "./component/Signin";
 import Header from "./component/Header";
-import Profile from "./component/Profile";
-import Outh from "./component/Outh";
 import Home from "./component/Home";
 function App() {
   return (
@@ -15,8 +13,6 @@ function App() {
           <Route exact path="/">
             <Redirect to="/signin" />
           </Route>
-          <Route path="/outh" component={Outh} />
-          <Route path="/profile" component={Profile} />
           <Route path="/home" component={Home} />
         </BrowserRouter>
       </main>
