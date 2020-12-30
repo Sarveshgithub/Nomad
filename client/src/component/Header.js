3f8eee933d54c6798e057da1c5f0bd966bf78f37
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-12-30 21:44:36 +0530

    add changes

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -11,7 +11,15 @@ function Header() {
         <h1>Nomad24</h1>
       </div>
       <div className="header-item">
-        <li>Log Issue</li>
+        <li>
+          <a
+            href="https://gitreports.com/issue/Sarveshgithub/Nomad"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Log Issue
+          </a>
+        </li>
       </div>
     </header>
   );
